8a3cd4463f594b53b04c4db9d65914e66a23e702
papillonyi <andyi@live.cn>
2016-03-16 13:57:37 +1100

    Initial Commit

---
+++ ELEC9772Track/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ELEC9772Track
+//
+//  Created by Andy on 16/03/2016.
+//  Copyright © 2016 Andy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
